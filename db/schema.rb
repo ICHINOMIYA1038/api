9a4ed3b4fab1a3ab21f305a93b4763a415aef523
ICHINOMIYA <ichiryo108@gmail.com>
2023-06-21 23:37:46 +0900

    post finish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_21_051128) do
-  create_table "posts", primary_key: "post_id", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "posts", primary_key: "post_id", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "content"
     t.string "image_id"
     t.datetime "createdAt"
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_051128) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", primary_key: "user_id", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", primary_key: "user_id", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_hash"
