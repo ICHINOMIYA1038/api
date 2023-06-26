5138f5a4dc24b0c885d14797161689a8bdd48b0c
ICHINOMIYA <ichiryo108@gmail.com>
2023-06-26 11:45:40 +0900

    626

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_051128) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_031253) do
   create_table "posts", primary_key: "post_id", id: :integer, charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.string "image_id"
@@ -30,18 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_051128) do
     t.datetime "deletedAt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.boolean "admin", default: false
   end
 
 end
