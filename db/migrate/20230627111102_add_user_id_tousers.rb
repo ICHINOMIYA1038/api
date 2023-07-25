f2d997a3bbc8acc94a93ddd922c6d9c28f07a305
ICHINOMIYA <ichiryo108@gmail.com>
2023-07-26 02:29:51 +0900

    deploy test

--- db/migrate/20230627111102_add_user_id_tousers.rb
+++ db/migrate/20230627111102_add_user_id_tousers.rb
@@ -1,15 +1,12 @@
 class AddUserIdTousers < ActiveRecord::Migration[7.0]
-  def up
-    Post.delete_all
-    User.delete_all
-  
+  def up  
     remove_column :users, :id
     # user_idカラムを追加し、PKとして設定
 
     #postgres用
-    add_column :users, :user_id, :serial, null: false, primary_key: true,AUTO_INCREMENT:true
+    #add_column :users, :user_id, :serial, null: false, primary_key: true,AUTO_INCREMENT:true
     #mysql
-    #add_column :users, :user_id, :integer, null: false, primary_key: true,AUTO_INCREMENT:true
+    add_column :users, :user_id, :integer, null: false, primary_key: true,AUTO_INCREMENT:true
     add_foreign_key :posts, :users, column: :user_id, primary_key: :user_id
   end
 
